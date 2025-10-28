21912682420df55ee0e58669aad923e015d0659d
Nathan Richardson Ávila Da Conceição <nathanrichardson.avila@outlook.com>
2025-10-28 16:16:41 -0300

    Mudança para seleção de interface de arquivo e carregamento

--- analise_vendas.py
+++ analise_vendas.py
@@ -2,12 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pathlib import Path
+import tkinter as tk
+from tkinter import filedialog
+import os
+
 
 #Configuração do dataset
 
+
+# Interface de seleção do arquivo
+
+def selecionar_arquivos_csv():
+    "Abre uma janela para o usuário escolher o arquivo CSV."
+    root = tk.Tk()
+    root.withdraw() #Oculta a janela principal
+    caminho_arquivo = filedialog.askopenfilename(
+        title="Selecione o arquivo CSV de vendas",
+        filetypes=[("Arquivos CSV", "*.csv")]
+    )
+    return caminho_arquivo
+
 #caminho
 
-arquivo_csv = Path(__file__).parent / "vendas.aleatorias.csv"
+arquivo_padrao = Path(__file__).parent / "vendas.aleatorias.csv"
+
+print(" Selecione um arquivo CSV: ")
+arquivo_csv = selecionar_arquivos_csv
+
+if not arquivo_csv:
+    arquivo_csv = arquivo_padrao
+    print(f"Nenhum arquivo selecionado. Usando padrão: {arquivo_csv.name}")
+else:
+    arquivo_csv = Path(arquivo_csv)
+    print(f"Arquivo selecionado: {os.path.basename(arquivo_csv)}")
 
 #leitura do dataset
 
@@ -59,6 +86,9 @@ print(faturamento_cat)
 produtos_mais_vendidos = df.groupby("produto")["quantidade"].sum().sort_values(ascending=False)
 print("\nTop 5 produtos mais vendidos:")
 print(produtos_mais_vendidos.head(5))
+
+# Visualização dos Gráficos
+
 
 
 
