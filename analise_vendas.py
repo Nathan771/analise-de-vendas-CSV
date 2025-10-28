34bb019ec757fbab92e7c43202a3da57938b13cb
Nathan Richardson Ávila Da Conceição <nathanrichardson.avila@outlook.com>
2025-10-28 15:45:33 -0300

    Atualização

--- analise_vendas.py
+++ analise_vendas.py
@@ -25,7 +25,7 @@ print(df.head())
 
 df["data"] = pd.to_datetime(df["data"], errors = "coerce")
 
-#Remvoer linhas com datas inválidas
+#Remover linhas com datas inválidas
 
 df = df.dropna(subset=["data"])
 
@@ -33,6 +33,32 @@ df = df.dropna(subset=["data"])
 
 df["quantidade"] = df["quantidade"].astype(int)
 df["preco_unitario"] = df["preco_unitario"].astype(float)
+
+#Criar coluna de faturamento
+
+df["faturamento"] = df["quantidade"] * df["preco_unitario"]
+
+#Analise descritivas
+
+print("\n Resumo estatístico:")
+print(df.describe())
+
+#Faturamento total
+
+faturamento_total = df["faturamento"].sum()
+print(f"\n Faturamento total: R$ {faturamento_total:,.2f}")
+
+#Categoria
+
+faturamento_cat = df.groupby("categoria")["faturamento"].sum().sort_values(ascending=False)
+print("\nFaturamento por categoria:")
+print(faturamento_cat)
+
+#Produtos mais vendidos
+
+produtos_mais_vendidos = df.groupby("produto")["quantidade"].sum().sort_values(ascending=False)
+print("\nTop 5 produtos mais vendidos:")
+print(produtos_mais_vendidos.head(5))
 
 
 
