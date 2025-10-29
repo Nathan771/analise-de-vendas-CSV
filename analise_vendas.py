34fd0d412b617e9b5b48352c64a0da93f5733431
Nathan Richardson Ávila Da Conceição <nathanrichardson.avila@outlook.com>
2025-10-29 17:54:03 -0300

    Feito alterações nos gráficos para maior interatividade e correções, código funcional.

--- analise_vendas.py
+++ analise_vendas.py
@@ -36,8 +36,6 @@ print(f"Total de registros: {len(df)}")
 print("\nVisualização inicial: ")
 print(df.head())
 
-#Limpeza e tratamento de dados
-
 
 #Converter datas
 
@@ -78,10 +76,15 @@ produtos_mais_vendidos = df.groupby("produto")["quantidade"].sum().sort_values(a
 print("\nTop 5 produtos mais vendidos:")
 print(produtos_mais_vendidos.head(5))
 
+#coluna de mês e faturamento mensal
+
+df ["mes"] = df["data"].dt.to_period("M").astype(str)
+faturamento_mensal = df.groupby("mes")["faturamento"].sum()
+
 # Visualização dos Gráficos
 
 sns.set(style = "whitegrid")
-figuras = []
+figs = []
 
 #Gráfico 1: Faturamento por categoria
 
@@ -90,7 +93,7 @@ sns.barplot(x=faturamento_cat.index, y = faturamento_cat.values, palette="viridi
 ax1.set_title("Faturamento por categoria")
 ax1.set_xlabel("Categoria")
 ax1.set_ylabel("Faturamento (R$)")
-figuras.append(fig1)
+figs.append(fig)
 
 """plt.figure(figsize=(8, 5))
 sns.barplot(x=faturamento_cat.index, y=faturamento_cat.values, palette = "viridis")
@@ -103,11 +106,18 @@ plt.show()
 #Gráfico 2: 5 Produtos mais vendidos
 
 fig2, ax2 = plt.subplots(figsize=(8, 5))
-sns.barplot(x=faturamento_cat.index, y=faturamento_cat.values, palette="viridis")
-ax1.set_title("Faturamento por categoria")
-ax1.set_xlabel("Categoria")
-ax1.set_ylabel("Faturamento (R$)")
-figuras.append(fig1)
+sns.barplot(
+    x=produtos_mais_vendidos.head(5).index,
+    y=produtos_mais_vendidos.head(5).values,
+    palette = "magma",
+    ax = ax2
+        
+)
+
+ax2.set_title("Top 5 produtos mais vendidos")
+ax2.set_xlabel("Produto")
+ax2.set_ylabel("Quantidade vendida")
+figs.append(fig2)
 
 """plt.figure(figsize=(8, 5))
 sns.barplot(
@@ -124,7 +134,17 @@ plt.show()
 """
 #Gráfico 3: Faturamento mensal
 
-df["mes"] = df["data"].dt.to.period("M").astype(str)
+fig3, ax3 = plt.subplots(figsize= (8, 5))
+sns.lineplot(x=faturamento_mensal.index, y=faturamento_mensal.values, marker = "o", color = "blue", ax = ax3)
+ax3.set_title("Faturamento mensal")
+ax3.set_xlabel("Mês")
+ax3.set_ylabel("Faturamento (R$)")
+
+for label in ax3.get_xticklabels():
+    label.set_rotation(45)
+figs.append(fig3)
+
+"""df["mes"] = df["data"].dt.to.period("M").astype(str)
 faturamento_mensal = df.groupby("mes")["faturamento"].sum()
 
 plt.figure(figsize = (8, 5))
@@ -135,6 +155,47 @@ plt.ylabel("Faturamento (R$)")
 plt.xticks(rotation = 45)
 plt.tight_layout()
 plt.show()
+"""
+# interface interativa
+
+root = tk.Tk()
+root.title("Análise de Vendas")
+root.geometry("1920x1080")
+
+indice_atual = 0
+canvas = None
+
+def mostrar_grafico():
+    global canvas
+    if canvas:
+        canvas.get_tk_widget().pack_forget()
+    fig = figs[indice_atual]
+    canvas = FigureCanvasTkAgg(fig, master = root)
+    canvas.draw()
+    canvas.get_tk_widget().pack(pady=20)
+
+def proximo():
+    global indice_atual
+    indice_atual = (indice_atual + 1) % len(figs)
+    mostrar_grafico()
+
+def anterior():
+    global indice_atual
+    indice = (indice - 1) % len(figs)
+    mostrar_grafico()
+
+frame_botoes = tk.Frame(root)
+frame_botoes.pack(pady=10)
+
+btn_ant = tk.Button(frame_botoes, text=" <- Anterior", width = 12, command=anterior)
+btn_ant.pack(side = "left", padx=10)
+
+btn_prox = tk.Button(frame_botoes, text="Proximo ->", width=12, command=proximo)
+btn_prox.pack(side="left", padx=10)
+
+mostrar_grafico()
+
+
 
 #Exportação 
 
@@ -143,6 +204,10 @@ faturamento_cat.to_excel(saida_excel)
 
 print(f"\n Relatório salvo em: {saida_excel}")
 
+
+#manter a janela aberta
+
+root.mainloop()
 
 
 
